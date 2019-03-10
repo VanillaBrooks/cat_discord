4ed7f942e69dcf311ca49f3e852e9cc55b33ac1e
VanillaBrooks <brookskarlik@gmail.com>
2019-03-10 01:24:26 -0800

    fixed DC problem, added logging

--- discord_bot.py
+++ discord_bot.py
@@ -1,7 +1,17 @@
-import discord, requests, os, time
+# outside libraries
+import discord
+import requests
 import praw
+
+# std lib
+import os
+import time
 import asyncio
-from api import dis_id, dis_secret, dis_token, permission_int
+import logging
+import datetime
+
+# local files
+from api import dis_id, dis_secret, dis_token, permission_int 
 from api import reddit_secret, reddit_client_id
 
 class MyClient(discord.Client):
@@ -13,75 +23,99 @@ class MyClient(discord.Client):
         r = praw.Reddit(client_id=reddit_client_id, client_secret=reddit_secret, user_agent='discord cat pictures by u/dehnextentaclesuprise')
 
         while True:
-            print(' in loop')
+            logging.info('Starting main while loop')
             submissions = [i.url for i in r.subreddit('cats').top('hour')]
+            logging.info('Submissions fetched')
+            
+            links = await check_link(submissions)
 
-            url = check_link(submissions)
-            download_image(url)
-
-            print('sending file')
+            if not await download_image(links):
+                continue
+                
+            logging.info('Trying to call send_the_file()')
             await self.send_the_file()
-            print('file sent')
+            logging.info('Exited send_the_file()')
             os.remove(self.file)
+            logging.info('deleted the file')
 
-            time.sleep(60*60) # hourly
+            logging.info('Sleeping...')
+            await asyncio.sleep(60*60) # hourly
 
     async def send_the_file(self):
         for channel in client.get_all_channels():
-            if 'cat' in channel.name:
-                print('sending a  file from loop')
+            if 'cat' == channel.name:
+                logging.info(f'Sending file to channel: {channel}')
                 await client.send_file(channel, self.file)
-                # await client.send_message(channel, 'this is a sample')
 
 
-def check_link(link_list):
+# filter function to find the links we can use 
+async def check_link(link_list):
+    good_links = []
     for link in link_list:
+        bad_link = True
         if "tumblr" not in link and 'imgur' not in link and 'gfycat' not in link and 'redd' \
                 not in link:
-            continue
+            pass
 
         # disregard if its an imgur .gif or .gifv
         elif '.gif' in link or '.gifv' in link or 'v.redd' in link:
-            continue
+            pass
 
         # dont download if its an imgur album or comments section
         elif '/a/' in link or '/r/' in link:
-            continue
+            pass
 
         elif link == True:
-            continue
+            pass
 
         # download gfycat posts
         elif 'gfycat' in link:
-            continue
+            pass
 
         # downlaod direct links to imgur and reddit
-        elif 'i.i' not in link and 'i.r' not in link and 'tumblr' not in link and \
-                'v.redd' not in link:
-            print('we found link')
-            return link
+        elif 'i.i' in link or 'i.r' in link:
+            logging.info(f'found good link: {link}')
+            good_links.append(link)
+            bad = False
+        if bad:
+            logging.info(f'BAD LINK : {link}')
 
-def download_image(link):
-    response = requests.get(link)
-    try:
-        with open('discord_file.png', 'wb') as fo:
-            for chunk in response.iter_content(4096):
-                fo.write(chunk)
+    return good_links
 
-    # oh fuck what went wrong better log that bitch
-    except Exception as e:
-        print('oh shit there was an exception with downloading')
+# iterates through list of good image links and tries to download one
+async def download_image(link_list):
+    for link in link_list:
+        logging.info(f'downloading image link {link}')
+        response = requests.get(link)
+        try:
+            with open('discord_file.png', 'wb') as fo:
+                for chunk in response.iter_content(4096):
+                    fo.write(chunk)
+
+            return True
+        # oh fuck what went wrong better log that bitch
+        except Exception:
+            logging.exception('Got an exception downloading')
+
         return False
 
-    return True
+# setup logging
+def logger_config():
+    logging_folder = 'logging/'
+    try: os.mkdir(logging_folder)
+    except Exception:
+        pass
+
+    # month - day __ hour - minute - second
+    LOG_FILENAME = logging_folder + datetime.datetime.now().strftime('%m-%d__%H-%M-%S') + '.txt'
+
+    logging.basicConfig(filename=LOG_FILENAME, 
+        level=logging.INFO, 
+        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+
 
 if __name__ == '__main__':
-    r = praw.Reddit(client_id=reddit_client_id, client_secret=reddit_secret, user_agent='discord cat pictures by u/dehnextentaclesuprise')
-
+    logger_config()
     client = MyClient()
-
-    # print(list(client.servers))
     client.run(dis_token)
-
-
-# https://discordapp.com/oauth2/authorize?&client_id=554126254429962242&scope=bot&permissions=8
