270cef617404ad98c0be3cef2660797cabe1cd3f
VanillaBrooks <brookskarlik@gmail.com>
2019-09-02 01:44:28 -0700

    encapsulation of CSV operations

--- discord_bot.py
+++ discord_bot.py
@@ -25,7 +25,7 @@ class MyClient(discord.Client):
 
         while True:
             # load a cache of the previous files
-            old_csv_data = await get_older_data()
+            csv = CsvData("old_data.csv")
 
             r = await parse_json.get_api(logging) # equal to headers from the get api
 
@@ -55,54 +55,119 @@ class MyClient(discord.Client):
             for link in submissions['url']:
                 num_posts_checked += 1
                 
-                #check that the dataframe exists
-                if type(old_csv_data) != bool:
+                if csv.contains_url(link) == False:
+                    # break out of loop if we successfully downloaded the image
+                    if await download_image(link) == True:
+                        break
+                else:
+                    continue
 
-                    # if the link is in the csv file we continue to the next iteration
-                    if link in old_csv_data['url'].values:
-                        continue
-                # break out of loop if we successfully downloaded the image
-                if await download_image(link) == True:
-                    break
-
-                     
             logging.info('Trying to call send_the_file()')
-            # await self.send_the_file()
+            await self.send_the_file()
             logging.info('Exited send_the_file()')
             os.remove(self.file)
             logging.info('deleted the file')
 
-            # remove checked posts from list and store them in csv
-            old_data = {}
-            old_data['id'] = []
-            old_data['url'] = []
-
             for _ in range(num_posts_checked):
-                old_data['id'].append(submissions['id'].pop(0))
-                old_data['url'].append(submissions['url'].pop(0))
+                id = submissions['id'].pop(0)
+                url = submissions['url'].pop(0)
 
-            # write to csv
-            df = pd.DataFrame(old_data)
+                csv.add_new_entry(id, url)            
 
-            df.to_csv('old_data.csv', header=True, index=False)
-
+            csv.save_csv()
 
             logging.info('Sleeping...')
-            await asyncio.sleep(60*60) # hourly
+            await asyncio.sleep(10) # hourly
 
     async def send_the_file(self):
         for channel in client.get_all_channels():
-            if 'cat' in channel.name:
+            if 'cat-stuff' in channel.name:
+            # if 'cat-worship' in channel.name:
                 logging.info(f'Sending file to channel: {channel}')
                 await client.send_file(channel, self.file)
                 # send_message
 
+# Handler for CSV operations        
+class CsvData():
+    def __init__(self, path):
+        self.path = path
 
-async def get_older_data():
-    if not os.path.exists('old_data.csv'):
-        return False
-    else:
-        return pd.read_csv('old_data.csv')
+        self.append_ids = []
+        self.append_urls = []
+
+        if os.path.exists(path):
+            self.check_csv = True
+
+            self.csv_data = pd.read_csv(path)
+        else: 
+            self.check_csv = False
+
+    # Aff new ids to be merged without checking if they are already in the DF
+    def add_new_entry_unchecked(self, id, url):
+        self.append_ids.append(id)
+        self.append_urls.append(url)
+    
+    # add new ids to be merged to the main DF while checking they dont already exist
+    def add_new_entry(self, id, url):
+        # data does not already exist
+        if self.contains_id(id) == False and self.contains_url(url) == False:
+            self.add_new_entry_unchecked(id, url)
+        # data exists
+        else:
+            pass
+
+    # id / url pair is BOTH not in the main dataframe
+    def contains_older(self, id, url):
+        
+        # csv contains the id or the url
+        if  self.contains_id(id) or self.contains_url(url):
+            return True
+        # the picture has not been posted before
+        else:
+            return False
+
+    # a given id is in the main dataframe
+    def contains_id(self, id):
+        if self.check_csv == True:
+            return self.csv_data['id'].isin([id]).any()
+        else: 
+            return False
+    # a given url is in the main dataframe
+    def contains_url(self, url):
+        if self.check_csv == True:
+            return self.csv_data['url'].isin([url]).any()
+        else: 
+            return False
+
+    # append current data to the CSV, merge the new ids and urls into the main dataframe
+    def save_csv(self):
+        new_df = pd.DataFrame({
+            "id": self.append_ids,
+            "url": self.append_urls
+            })
+        
+        if self.check_csv == False:
+            # this is the creation of the CSV, write the headers
+            new_df.to_csv(self.path, header=True, index=False)
+            self.check_csv = True
+            self.csv_data = new_df
+
+        elif self.check_csv == True:
+            print("appending")
+            print(new_df)
+            # Append to existing data, no headers
+            new_df.to_csv(self.path, mode='a', header=False, index=False)
+            self.csv_data = pd.concat([self.csv_data, new_df])
+
+        self.append_ids  = []
+        self.append_urls = []
+        
+    def __repr__(self):
+        if self.check_csv:
+            return str(self.csv_data)
+        else:
+            return ""
+            
 
 # iterates through list of good image links and tries to download one
 async def download_image(link):
